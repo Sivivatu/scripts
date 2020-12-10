32e7956002baa2494722cc976da2a4d81c02e10f
Paul <Sivivatu@users.noreply.github.com>
2020-12-10 13:21:27 +0000

    testing archive changes

--- Python Scripts/OMG_screenshot_cleanup_2.py
+++ Python Scripts/OMG_screenshot_cleanup_2.py
@@ -14,26 +14,27 @@ screenshots_archive = os.path.join(baseOMG, "screenshots_2.7z")
 os.chdir(screenshots)
 
 file_names = os.listdir(screenshots)
-# backup_location = os.path.join(screenshots, archive_date)
+backup_location = os.path.join(screenshots, archive_date)
 
-# try:
-#     os.mkdir(backup_location)
-# except OSError as e:
-#     if e.errno != errno.EEXIST:
-#         raise
+try:
+    os.mkdir(backup_location)
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        raise
 
-# for file_name in file_names:
-#     shutil.move(os.path.join(screenshots, file_name), backup_location)
+for file_name in file_names:
+    shutil.move(os.path.join(screenshots, file_name), backup_location)
 
 # # print(screenshots)
 
-# os.chdir(baseOMG)
+os.chdir(baseOMG)
 # directory = os.path.dirname(file_path)
 
 # # file_names = os.listdir(os.path.join(screenshots, archive_date))
-with py7zr.SevenZipFile(screenshots_archive, 'w') as z:
-    z.writeall(file_names)
-#     file_names[i] = os.path.join(screenshots, file_names[i])
+# for file_name in file_names:
+#     with py7zr.SevenZipFile(screenshots_archive, 'a') as z:
+#         z.write(file_name, archive_date)
+# #     file_names[i] = os.path.join(screenshots, file_names[i])
 
 # # Add to the backup zip
 # with py7zr.SevenZipFile(screenshots_archive, 'a') as z:
