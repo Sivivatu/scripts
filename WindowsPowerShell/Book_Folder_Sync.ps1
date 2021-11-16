750043de0b3ecc0d37545f861c40d1a1000034b4
Paul <Sivivatu@users.noreply.github.com>
2021-11-16 18:45:31 +0000

    changed log save location

--- WindowsPowerShell/Book_Folder_Sync.ps1
+++ WindowsPowerShell/Book_Folder_Sync.ps1
@@ -5,8 +5,9 @@ $dest_googledrive_book = "H:\My Drive\Book\Alteryx for Data Engineers\Book Chapt
 $files = "*.docx" # files in folder to copy
 $copy_control = "/COPYALL /B" # is supposed to take all document properties
 $options = "/NDL" # /R:2 
+$logfile = "C:\Users\paulj\OneDrive\Documents\scripts\WindowsPowerShell\logs\synclog.txt"
 
 # example command to reference
 # robocopy "$source $dest $what $options /LOG:MyLogfile.txt"
 Write-Host "Test sync : Robocopy.exe $source_onedrive_book $dest_googledrive_book $files $copy_control $options /LOG+:synclog.txt"
-Robocopy.exe "$source_onedrive_book" "$dest_googledrive_book" $files /LOG+:synclog.txt #$copy_control # $options # 
+Robocopy.exe "$source_onedrive_book" "$dest_googledrive_book" $files /LOG+:$logfile #$copy_control # $options # 
