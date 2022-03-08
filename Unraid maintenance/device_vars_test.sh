804aca61b12d256a7aa592c3b2940832ec8ad7b8
Paul <Sivivatu@users.noreply.github.com>
2022-03-08 19:17:21 +0000

    test file for unraid user script

---
+++ Unraid maintenance/device_vars_test.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+#description=this is the description of the script
+#foregroundOnly=true
+#backgroundOnly=true
+#arrayStarted=true
+#name=blah blah blah
+#clearLog=true
+#argumentDescription=This is the description of the argument(s)
+#argumentDefault=defaultArg1 defaultArg2 etc
+
+#noParity=true
+
+
+for var in "$@"
+do
+    echo "var: $var"
+done
