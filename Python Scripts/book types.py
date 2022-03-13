3ca3f8791eb57795643cf9b45167baa5b51de3ee
Paul <Sivivatu@users.noreply.github.com>
2022-03-13 20:13:18 +0000

    testing book list in folder

---
+++ Python Scripts/book types.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+from pandas import DataFrame
+
+root = Path("\\\\ROOKERY\\Media\\Books")
+print(root)
+
+file_list = []
+
+for file in os.walk(root):
+    file_list.append(file)
+
+df = DataFrame(file_list, columns = ["root", "dirs", "files"])
+
+df.head()
+# # file_list = os.walk("directory")
+# print(file_list[5])
+
+# for root, dirs, files in os.walk(root):
+#     for name in files:
+#         print(os.path.join(root, name))
