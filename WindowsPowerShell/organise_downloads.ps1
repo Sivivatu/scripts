67cd239a04a7d5da4fab8c2016b7e0eee9560fbf
Paul <Sivivatu@users.noreply.github.com>
2023-05-19 20:10:41 +0100

    initial organise process from ChatGPT

---
+++ WindowsPowerShell/organise_downloads.ps1
@@ -0,0 +1,120 @@
+param (
+    [string]$downloadFolder = "C:\Users\paulj\Downloads\download_test",
+    [int]$daysToArchive = 14
+)
+
+# Define file extensions and their corresponding folder names
+$extensionMapping = @{
+    # Alteryx Files
+    ".yxdb" = "Alteryx Files"
+    ".yxmc" = "Alteryx Files"
+    ".yxmd" = "Alteryx Files"
+    ".yxi" = "Alteryx Files"
+    
+    # Archive Files
+    ".zip" = "Archives"
+    ".iso"  = "Archives"
+    ".7z"  = "Archives"
+    ".tar"  = "Archives"
+    ".gz"  = "Archives"
+    ".msi"  = "Archives"
+    ".exe"  = "Archives"
+    
+    # Images
+    ".jpg" = "Images"
+    ".jpeg"	= "Images"
+    ".jpe" = "Images"
+    ".jif" = "Images"
+    ".jfif" = "Images"
+    ".jfi"	= "Images"
+    ".png" = "Images"
+    ".gif"	= "Images"
+    ".webp"	= "Images"
+    ".tiff"	= "Images"
+    ".tif"	= "Images"
+    ".psd"	= "Images"
+    ".raw"	= "Images"
+    ".arw" = "Images"
+    ".cr2"	= "Images"
+    ".nrw"	= "Images"
+    ".k25"	= "Images"
+    ".bmp"	= "Images"
+    ".dib"	= "Images"
+    ".heif"	= "Images"
+    ".heic" = "Images"
+    ".ind"	= "Images"
+    ".indd"	= "Images"
+    ".indt"	= "Images"
+    ".jp2"	= "Images"
+    ".j2k"	= "Images"
+    ".jpf"	= "Images"
+    ".jpx"	= "Images"
+    ".jpm"	= "Images"
+    ".mj2"	= "Images"
+    ".svg"	= "Images"
+    ".svgz"	= "Images"
+    ".ai"	= "Images"
+    ".eps" = "Images"
+    ".ico" = "Images"
+    
+    # Documents and spreadsheets
+    ".pdf" = "Document Files"
+    ".txt" = "Document Files"
+    ".doc" = "Document Files"
+    ".docx" = "Document Files"
+    ".odt" = "Document Files"
+    ".csv" = "Document Files"
+    ".xls" = "Document Files"
+    ".xlsx" = "Document Files"
+    ".ppt" = "Document Files"
+    ".pptx" = "Document Files"
+    
+    # Add more extensions and folder names as needed
+}
+
+# Create folders if they don't exist
+foreach ($folder in $extensionMapping.Values) {
+    $folderPath = Join-Path -Path $downloadFolder -ChildPath $folder
+    if (-not (Test-Path -Path $folderPath -PathType Container)) {
+        New-Item -Path $folderPath -ItemType Directory | Out-Null
+    }
+}
+
+# Move files to the appropriate folders
+Get-ChildItem -Path $downloadFolder | ForEach-Object {
+    $extension = $_.Extension
+    if ($extensionMapping.ContainsKey($extension)) {
+        $destinationFolder = Join-Path -Path $downloadFolder -ChildPath $extensionMapping[$extension]
+        Move-Item -Path $_.FullName -Destination $destinationFolder
+    }
+}
+
+# Determine the cutoff date for archiving
+$cutoffDate = (Get-Date).AddDays(-$daysToArchive)
+
+# Create the zip folder name based on the current month and year
+$zipFolderName = "Archive_" + (Get-Date).ToString("yyyy_MM")
+
+# Create the zip folder path
+$zipFolderPath = Join-Path -Path $downloadFolder -ChildPath $zipFolderName
+
+# Create the zip folder if it doesn't exist
+if (-not (Test-Path -Path $zipFolderPath -PathType Container)) {
+    New-Item -Path $zipFolderPath -ItemType Directory | Out-Null
+}
+
+# Get the files to archive based on the cutoff date
+$filesToArchive = Get-ChildItem -Path $downloadFolder | Where-Object { $_.LastWriteTime -lt $cutoffDate }
+
+# Create the zip file path
+$zipFilePath = Join-Path -Path $zipFolderPath -ChildPath ($zipFolderName + ".zip")
+
+# Archive the files
+if ($filesToArchive.Count -gt 0) {
+    $shellApp = New-Object -ComObject Shell.Application
+    $zipFile = $shellApp.NameSpace($zipFilePath)
+    $filesToArchive | ForEach-Object {
+        $zipFile.CopyHere($_.FullName)
+    }
+}
+
